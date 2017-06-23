f6040fd2414dd3a4a4fd454f6890fc635124107b
Konrad Eisele <eiselekd@gmail.com>
2017-06-23 16:30:51 +0000

    all

--- Makefile.pre.mk
+++ Makefile.pre.mk
@@ -6,6 +6,12 @@ prepare:
 tool:
 	if [ ! -d esp-open-sdk ] ; then git clone --recursive https://github.com/pfalcon/esp-open-sdk.git;  fi
 	cd esp-open-sdk; make STANDALONE=y
+	mv esp-open-sdk/xtensa-lx106-elf/bin/esptool.py esp-open-sdk/xtensa-lx106-elf/bin/esptool.py.bck;
+	git clone https://github.com/espressif/esptool
+
+# git@github.com:eiselekd/esptool.git
+esptool:
+	git clone https://github.com/espressif/esptool; 
 
 tool-tar:
 	cd esp-open-sdk; tar czvf sdk.tar.gz sdk/* xtensa-lx106-elf
